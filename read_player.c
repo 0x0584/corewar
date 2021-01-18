9b6b0c2c5faa3d18e25279e4c73713611a8d9ed9
Anas Rchid <archid-@student.1337.ma>
2021-01-18 11:03:40 +0100

    add read_player.c

--- read_player.c
+++ read_player.c
@@ -1,9 +1,7 @@
 #include "libft.h"
 #include "op.h"
 
-static inline t_u8 bebyte(t_u8 byte) {
-	return (byte << 2 | byte >> 2);
-}
+static inline t_u8 bebyte(t_u8 byte) { return (byte << 2 | byte >> 2); }
 
 /* Big Endian Word -- 32-bit */
 static inline t_u32 beword(t_u32 word) {
@@ -15,25 +13,37 @@ int main(int argc, char *argv[]) {
     t_header hdr;
     ssize_t readsz;
 
-	/* --------------------------------------------- */
+    /* --------------------------------------------- */
 
     if ((readsz = read(STDIN_FILENO, &hdr, sizeof(t_header))) < 0)
         return -1;
     else if (readsz != sizeof(t_header))
         return ft_dprintf(2, "header is corrupted\n");
-	else if (hdr.magic != beword(COREWAR_EXEC_MAGIC))
-		return ft_dprintf(2, "file format unknown: %x\n", hdr.magic);
 
-	/* --------------------------------------------- */
+    /* --------------------------------------------- */
 
-	hdr.prog_name[PROG_NAME_LENGTH] = '\0';
-	hdr.comment[COMMENT_LENGTH] = '\0';
-	ft_printf("prog name: %s\n", hdr.prog_name);
-	ft_printf("prog size: %u Byte\n", beword(hdr.prog_size));
-	ft_printf("prog size: %s\n", hdr.comment);
+    hdr.magic = beword(hdr.magic);
+    hdr.prog_size = beword(hdr.prog_size);
 
-	/* --------------------------------------------- */
+    /* --------------------------------------------- */
 
+    t_u8 *cor = malloc(hdr.prog_size);
+
+    if (hdr.magic != COREWAR_EXEC_MAGIC)
+        return ft_dprintf(2, "file format unknown: %x\n", hdr.magic);
+    else if ((readsz = read(STDIN_FILENO, cor, hdr.prog_size)) < 0)
+        return -1;
+    else if (readsz != hdr.prog_size)
+        return ft_dprintf(2, "binary is altered, prog_size: %u != %ld\n",
+                          hdr.prog_size, readsz);
+
+    /* --------------------------------------------- */
+
+    ft_printf("prog name: %s\n", hdr.prog_name);
+    ft_printf("prog size: %u Byte\n", hdr.prog_size);
+    ft_printf("prog size: %s\n", hdr.comment);
+
+    /* --------------------------------------------- */
 
     return 0;
 }
