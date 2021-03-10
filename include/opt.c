ed77449179c23bc04ddb6cbd659af095afcf2446
Anas Rchid <archid-@student.1337.ma>
2021-03-10 15:12:45 +0100

    add opt_dump

--- include/opt.c
+++ include/opt.c
@@ -1,8 +1,35 @@
+#include "const.h"
 #include "opt.h"
 
-static inline void	match_opt(void *blob, void *arg)
+static void			match_opt(void *blob, void *arg)
 {
+	t_opt				*opt;
+	t_pair				*pair;
 
+	pair = arg;
+	if (!(pair->first && pair->second))
+		return ;
+	opt = blob;
+	if (!ft_strcmp(opt->long_opt, pair->first))
+		pair->first = NULL;
+	if (*(char *)pair->second == opt->short_opt)
+		pair->second = NULL;
+}
+
+static bool			duplicate_opt(const char *long_opt, char short_opt)
+{
+	t_pair pair;
+
+	pair = (t_pair){(char *)long_opt, &short_opt};
+	lst_iter_arg(g_opts, true, &pair, match_opt);
+	return (!pair.first || !pair.second);
+}
+
+bool					opt_str_init(const char *long_opt, char short_opt,
+									 opt_arg_callback parse)
+{
+	return opt_init(long_opt, short_opt, true,
+					(t_opt_arg){parse, free, str_dump, NULL});
 }
 
 bool				opt_init(const char *long_opt, char short_opt,
@@ -12,17 +39,19 @@ bool				opt_init(const char *long_opt, char short_opt,
 
 	if (!g_opts)
 		g_opts = lst_alloc(arg.free);
-
+	else if (duplicate_opt(long_opt, short_opt))
+		return (false);
 	opt.long_opt = long_opt;
 	opt.short_opt = short_opt;
 	opt.has_arg = has_arg;
 	ft_memcpy(&opt.arg, &arg, sizeof(t_opt_arg));
-	return false;
+	return true;
 }
 
-bool				opt_match(const char *cmd_arg, t_opt *opt)
+bool					opt_match(const char *args[], int at, t_opt *opt)
 {
-	return false;
+	if (*args[0] != '-')
+		return false;
 }
 
 void				opt_cleanup(void)
